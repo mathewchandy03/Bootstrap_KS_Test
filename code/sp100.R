b94a9a4d7a3f47590fe4acf692b6097f20652304
Mathew Chandy <mathewchandy03@gmail.com>
2024-04-02 18:58:15 -0400

    manuscript tidying up

--- code/sp100.R
+++ code/sp100.R
@@ -5,59 +5,45 @@ library(tseries)
 library(extraDistr)
 library(xtable)
 
-sp100_pval_5y <- app_scheme("2019-01-01", "2023-12-31", 5, "^sp100")
+#sp100_pval_5y <- app_scheme("2019-01-01", "2023-12-31", 5, "^sp100")
 
 sp100_pval_4y <- app_scheme("2020-01-01", "2023-12-31", 4, "^sp100")
 
-sp100_pval_3y <- app_scheme("2021-01-01", "2023-12-31", 3, "^sp100")
+#sp100_pval_3y <- app_scheme("2021-01-01", "2023-12-31", 3, "^sp100")
 
-sp100_pval_2y <- app_scheme("2022-01-01", "2023-12-31", 2, "^sp100")
+#sp100_pval_2y <- app_scheme("2022-01-01", "2023-12-31", 2, "^sp100")
 
-sp100_pval_1y <- app_scheme("2023-01-01", "2023-12-31", 1, "^sp100")
+#sp100_pval_1y <- app_scheme("2023-01-01", "2023-12-31", 1, "^sp100")
 
-sp100_df <- t(rbind(sp100_pval_1y, sp100_pval_2y, sp100_pval_3y, sp100_pval_4y, 
-                    sp100_pval_5y
-))
+#sp100_df <- t(rbind(sp100_pval_1y, sp100_pval_2y, sp100_pval_3y, sp100_pval_4y, 
+#                    sp100_pval_5y
+#))
 
-saveRDS(sp100_df, "../data/sp100_df")
+saveRDS(sp100_pval_4y, "../data/sp100_pval_4y")
 
-write(print(xtable(sp100_pval_1y,
-                   caption = "P-values for 1 year of SP100 stock return data using different 
-  methods
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP1001"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp100_pval_1y.tex")
+my_format <- function(x) {
+  format(round(x, 4), nsmall = 4)
+}
 
-write(print(xtable(sp100_pval_2y,
-                   caption = "P-values for 2 years of SP100 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP1002"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp100_pval_2y.tex")
+sp100_pval_4y[,2] <- sapply(as.numeric(sp100_pval_4y[,2]), my_format)
 
-write(print(xtable(sp100_pval_3y,
-                   caption = "P-values for 3 years of SP100 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP1003"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp100_pval_3y.tex")
+sp100_pval_4y[,3] <- sapply(as.numeric(sp100_pval_4y[,3]), my_format)
+
+sp100_pval_4y[,4] <- sapply(as.numeric(sp100_pval_4y[,4]), my_format)
+
+colnames(sp100_pval_4y) <- c("$v$", "Our Method", "Babu's Method",
+                             "Zeimbekakis' Method")
+ 
+sp100_pval_4y[1, 1] <- "$\\infty$"
 
 write(print(xtable(sp100_pval_4y,
-                   caption = "P-values for 4 years of SP100 stock return 
+                   caption = "P-values for 4 years of S\\&P 100 stock return 
                    data using different durations
   and different degrees of freedom for Student's t distribution.",
-  label = "table:SP1004"
-), caption.placement = "top"), 
+  label = "table:SP1004",
+  digits = 3
+), caption.placement = "top", sanitize.text.function=function(x){x},
+include.rownames = FALSE), 
 file = "../manuscript/tables/sp100_pval_4y.tex")
 
-write(print(xtable(sp100_pval_5y,
-                   caption = "P-values for 5 years of SP100 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP1005"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp100_pval_5y.tex")
 
--- code/sp500.R
+++ code/sp500.R
@@ -5,59 +5,45 @@ library(tseries)
 library(extraDistr)
 library(xtable)
 
-sp500_pval_5y <- app_scheme("2019-01-01", "2023-12-31", 5, "^gspc")
+#sp500_pval_5y <- app_scheme("2019-01-01", "2023-12-31", 5, "^gspc")
 
 sp500_pval_4y <- app_scheme("2020-01-01", "2023-12-31", 4, "^gspc")
 
-sp500_pval_3y <- app_scheme("2021-01-01", "2023-12-31", 3, "^gspc")
+#sp500_pval_3y <- app_scheme("2021-01-01", "2023-12-31", 3, "^gspc")
 
-sp500_pval_2y <- app_scheme("2022-01-01", "2023-12-31", 2, "^gspc")
+#sp500_pval_2y <- app_scheme("2022-01-01", "2023-12-31", 2, "^gspc")
 
-sp500_pval_1y <- app_scheme("2023-01-01", "2023-12-31", 1, "^gspc")
+#sp500_pval_1y <- app_scheme("2023-01-01", "2023-12-31", 1, "^gspc")
 
-sp500_df <- t(rbind(sp500_pval_1y, sp500_pval_2y, sp500_pval_3y, sp500_pval_4y, 
-                        sp500_pval_5y
-))
+#sp500_df <- t(rbind(sp500_pval_1y, sp500_pval_2y, sp500_pval_3y, sp500_pval_4y, 
+#                        sp500_pval_5y
+#))
 
-saveRDS(sp500_df, "../data/sp500_df")
+saveRDS(sp500_pval_4y, "../data/sp500_pval_4y")
 
-write(print(xtable(sp500_pval_1y,
-                   caption = "P-values for 1 year of SP500 stock return data using different 
-  methods
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP5001"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp500_pval_1y.tex")
+my_format <- function(x) {
+  format(round(x, 4), nsmall = 4)
+}
 
-write(print(xtable(sp500_pval_2y,
-                   caption = "P-values for 2 years of SP500 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP5002"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp500_pval_2y.tex")
+sp500_pval_4y[,2] <- sapply(as.numeric(sp500_pval_4y[,2]), my_format)
 
-write(print(xtable(sp500_pval_3y,
-                   caption = "P-values for 3 years of SP500 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP5003"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp500_pval_3y.tex")
+sp500_pval_4y[,3] <- sapply(as.numeric(sp500_pval_4y[,3]), my_format)
+
+sp500_pval_4y[,4] <- sapply(as.numeric(sp500_pval_4y[,4]), my_format)
+
+colnames(sp500_pval_4y) <- c("$v$", "Our Method", "Babu's Method",
+                             "Zeimbekakis' Method")
+
+sp500_pval_4y[1, 1] <- "$\\infty$"
 
 write(print(xtable(sp500_pval_4y,
-                   caption = "P-values for 4 years of SP500 stock return 
+                   caption = "P-values for 4 years of S\\&P 500 stock return 
                    data using different durations
   and different degrees of freedom for Student's t distribution.",
-  label = "table:SP5004"
-), caption.placement = "top"), 
+  label = "table:SP5004",
+  digits = 3
+), caption.placement = "top", sanitize.text.function=function(x){x},
+include.rownames = FALSE), 
 file = "../manuscript/tables/sp500_pval_4y.tex")
 
-write(print(xtable(sp500_pval_5y,
-                   caption = "P-values for 5 years of SP500 stock return 
-                   data using different durations
-  and different degrees of freedom for Student's t distribution.",
-  label = "table:SP5005"
-), caption.placement = "top"), 
-file = "../manuscript/tables/sp500_pval_5y.tex")
 
