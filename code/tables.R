71c159ad061761372372ef42056387bd63af25b1
Mathew Chandy <mathewchandy03@gmail.com>
2024-05-18 19:39:02 -0400

    changed table

--- code/tables.R
+++ code/tables.R
@@ -1,9 +1,11 @@
 library(xtable)
+library(tidyverse)
 df <- readRDS("../data/null_rejection_rates.RDS") %>% 
   mutate(rr_lb = round(as.numeric(rr_lb), 3),
          rr = round(as.numeric(rr), 4),
          rr_ub = round(as.numeric(rr_ub), 3),
-         alpha = format(round(as.numeric(alpha), 2), nsmall = 2))
+         alpha = format(round(as.numeric(alpha), 2), nsmall = 2),
+         `Empirical Size` = rr / as.numeric(alpha))
 
 df$tau <- factor(case_when(df$phi == "-0.9238795" ~ "$\\tau = -0.75$",
                            df$phi == "-0.7071068" ~ "$\\tau = -0.5$",
@@ -22,13 +24,13 @@ df$tau <- factor(case_when(df$phi == "-0.9238795" ~ "$\\tau = -0.75$",
 
 library(reshape2)
 df_norm <- df %>% filter(dist == "normal") %>% 
-  dplyr::select(n, alpha, rr, tau) %>% 
-  dcast(n + alpha ~ tau, value.var = "rr") %>% 
+  dplyr::select(n, alpha, rr, tau, `Empirical Size`) %>% 
+  dcast(n + alpha ~ tau, value.var = "Empirical Size") %>% 
   rename(`$n$` = n, `$\\alpha$` = alpha)
 
 write(print(xtable(df_norm, 
-                   caption = "Rejection rates for test that $N(8, 8)$ is indeed
-                   normally distributed for
+                   caption = "Empirical sizes for the test that sample follows its true
+                    distribution family for
                    different values of AR(1) coefficient and for different 
                    significance levels.",
                    label = "table:rr_norm", digits = 4), caption.placement = "top",
@@ -36,14 +38,13 @@ write(print(xtable(df_norm,
       file = "../manuscript/tables/rr_norm.tex")  
 
 df_gamma <- df %>% filter(dist == "gamma") %>% 
-  dplyr::select(n, alpha, rr, tau) %>% 
-  dcast(n + alpha ~ tau, value.var = "rr") %>% 
+  dplyr::select(n, alpha, rr, tau, `Empirical Size`) %>% 
+  dcast(n + alpha ~ tau, value.var = "Empirical Size") %>% 
   rename(`$n$` = n, `$\\alpha$` = alpha)
 
 write(print(xtable(df_gamma, 
-                   caption = "Rejection rates for test that $\\Gamma(8, 8)$ is 
-                   indeed
-                   gamma-distributed for
+                   caption = "Empirical sizes for the test that sample follows its true
+                    distribution family for
                    different values of AR(1) coefficient and for different 
                    significance levels.",
                    label = "table:rr_gamma", digits = 4), caption.placement = "top",
